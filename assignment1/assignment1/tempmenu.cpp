12bb4e281accd630a70cb5bd45034a9f675681c5
Jordan Ma <88352172+Ma-Jordan@users.noreply.github.com>
2021-08-24 16:28:54 -0700

    Add files via upload

---
+++ assignment1/assignment1/tempmenu.cpp
@@ -0,0 +1,7 @@
+/*cout << "Menu for deposits:\n"
+<< "\t 'q' - deposit a Quater\n"
+<< "\t 'd' - deposit a Dime\n"
+<< "\t 'n' - deposit a Nickel\n"
+<< "\t 'o' - deposit a Dollar Bill\n"
+<< "\t 'f' - deposit a Five Dollar Bill\n"
+<< "\t 'c' - cancel the purchase\n";*/
